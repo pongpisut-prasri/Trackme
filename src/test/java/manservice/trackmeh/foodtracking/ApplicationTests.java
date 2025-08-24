726b367ad9f3319e990f2e9a70776991250d9461
GodManV2 <manthesplit508@gmail.com>
2025-08-24 13:27:42 +0700

    initialProject

---
+++ src/test/java/manservice/trackmeh/foodtracking/ApplicationTests.java
@@ -0,0 +1,13 @@
+package manservice.trackmeh.foodtracking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
